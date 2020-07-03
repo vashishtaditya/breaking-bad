7eea307c7ebb2579b32d0d6a6c26b1ed777227b6
Aditya Vashisht <vash0005@algonquinlive.com>
2020-07-03 00:52:22 -0400

    Updates

---
+++ precache-manifest.b7794805d27c7e13ddf24febbb95a1fe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1366eeab05c1a89d42c23bbfc90e30e1",
+    "url": "/breaking-bad/index.html"
+  },
+  {
+    "revision": "556134f4a8884d4ea7f9",
+    "url": "/breaking-bad/static/css/main.205a88cc.chunk.css"
+  },
+  {
+    "revision": "a8270ff655e83b055f97",
+    "url": "/breaking-bad/static/js/2.b8e677f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad/static/js/2.b8e677f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "556134f4a8884d4ea7f9",
+    "url": "/breaking-bad/static/js/main.365e3599.chunk.js"
+  },
+  {
+    "revision": "c3ab8afeda08e5f08bd0",
+    "url": "/breaking-bad/static/js/runtime-main.09eddc7a.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking-bad/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking-bad/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breaking-bad/static/media/spinner.235b9b3e.gif"
+  }
+]);
